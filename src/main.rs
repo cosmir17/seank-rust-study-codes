cd13cd34a3fe73f50c5e6c792b7b1fe6f9aeed3e
cosmir17 <ks1537@gmail.com>
2024-04-02 22:37:25 +0100

    added a comment in src/main.rs file and updated .gitignore file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    // The project requires this specific file in order to successfully compile.
+    // The project is organized into four distinct directories excluding the folder where this file is in.
+    // For further information, please consult the readme.md file.
+    println!("Hello, world!"); 
 }
