5c92066a8f03e1183fb39f2d74c2b32d9dd33bdf
cosmir17 <ks1537@gmail.com>
2024-06-08 20:58:38 +0100

    another codewars solution

--- codewars_kata_training/examples/main.rs
+++ codewars_kata_training/examples/main.rs
@@ -16,6 +16,34 @@ mod array_diff_tests {
     }
 }
 
+#[cfg(test)]
+mod like_description_text_tests {
+    use either::Either;
+    fn likes(names: &[&str]) -> String {
+        match names.len() {
+            0 => "no one likes this".to_string(),
+            1 => format!("{} likes this", names[0]),
+            2 => format!("{} and {} like this", names[0], names[1]),
+            3 => format!("{}, {} and {} like this", names[0], names[1], names[2]),
+            _ => format!("{}, {} and {} others like this", names[0], names[1], names.len() - 2),
+        }
+    }
+
+    #[test]
+    fn example_tests() {
+        assert_eq!(likes(&[]), "no one likes this");
+        assert_eq!(likes(&["Peter"]), "Peter likes this");
+        assert_eq!(likes(&["Jacob", "Alex"]), "Jacob and Alex like this");
+        assert_eq!(
+            likes(&["Max", "John", "Mark"]),
+            "Max, John and Mark like this"
+        );
+        assert_eq!(
+            likes(&["Alex", "Jacob", "Mark", "Max"]),
+            "Alex, Jacob and 2 others like this"
+        );
+    }
+}
 
 #[cfg(test)]
 mod disem_vowel_tests {
